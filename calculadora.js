35d6b2621d8a1d17191538c7134aa4232a4688f0
Gael <gael027gg@gmail.com>
2026-01-20 15:11:12 -0600

    agrega ejercicio calculadroa

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+//alert("Hola mundo hijo puta ");
+
+function mostrar(){
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+
+    let resultado = 0;
+    resultado = numero1 +numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = resultado;
+    
+    //alert(resultado)
+
+}
